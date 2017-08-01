73a282dd76d88191a6f843c9c9392a443a401e1c
piano-man <icm2015003@iiita.ac.in>
2017-08-01 18:52:46 +0530

    text replacement with spoiler added

--- search.js
+++ search.js
@@ -3,14 +3,17 @@ chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
   // Search/Highlight code goes here
   console.log("in search.js")
   var elements = document.getElementsByTagName('*');
+  var divs = document.getElementById(/substream/g);
 
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
+    //console.log(element.childNodes)
 
     for (var j = 0; j < element.childNodes.length; j++) {
         var node = element.childNodes[j];
 
         if (node.nodeType === 3) {
+            console.log(node.parentNode)
             var text = node.nodeValue;
             var find = message.searchText;
             var re = new RegExp(find, 'gi');
@@ -22,5 +25,4 @@ for (var i = 0; i < elements.length; i++) {
         }
     }
 }
-  console.log(message.searchText)
 });
